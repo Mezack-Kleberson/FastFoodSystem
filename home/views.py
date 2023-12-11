aae5b202d924841e9376e8dd9b2943411b2755bd
Mezack-Kleberson <mezackkleberson@gmail.com>
2023-12-11 20:24:40 -0300

    Add project github

---
+++ home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(
+        request,
+        'home/index.html'
+    )
+
+def cart(request):
+    return render(
+        request,
+        'home/cart.html'
+    )
